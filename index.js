4d0c1dae6f75b1db15f2533e1e969186c09ac260
Saad Zafar <31594740+saadie123@users.noreply.github.com>
2018-04-29 15:27:17 +0500

    Server and view engine setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const hbs = require('hbs');
+const expresshbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('.hbs', expresshbs({ defaultLayout: 'layout', extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname,'public')));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true }));
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`App runnnig on port ${port}`);
+});
