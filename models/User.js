c7d51eb3e68b2655e7b0fdb673ef220578484d77
Saad Zafar <31594740+saadie123@users.noreply.github.com>
2018-04-30 07:00:31 +0500

    Added partials and user model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+        validate:{
+            validator: (value) => {
+                return validator.isEmail(value);
+            },
+            message: '{VALUE} is not a valid email address'
+        }
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    tweets: [{
+        tweet: { type: mongoose.SchemaTypes.ObjectId, ref: 'tweets'}
+    }]
+});
+
+module.exports = mongoose.model('users', userSchema);
