50caa433797aa14c45be438a49a4355013d858af
Saad Zafar <31594740+saadie123@users.noreply.github.com>
2018-05-01 15:56:39 +0500

    Added tweet save functionality

--- realtime/io.js
+++ realtime/io.js
@@ -1,9 +1,32 @@
+const async = require('async');
+const Tweet = require('../models/Tweet');
+const User = require('../models/User');
 module.exports = (io) => {
     io.on('connect', (socket) => {
         console.log("User Connected");
-
+        const user = socket.request.user
         socket.on('tweet', (data)=>{
-            io.emit('message', {tweet:data, user: socket.request.user});
+            async.parallel([
+                function (callback) {
+                    io.emit('message', {tweet:data, user});
+                },
+                function (callback) {
+                    const tweet = new Tweet({
+                        content: data.content,
+                        author: user.id
+                    });
+                    tweet.save().then(tweet => {
+                        callback(tweet);
+                    });
+                },
+                function (tweet) {
+                    User.findByIdAndUpdate(user.id,{$push: {tweets: {tweet:tweet.id}}},{new: true})
+                    .then(updatedUser=>{
+                        console.log("tweet saved");
+                        tweet(updatedUser);
+                    });
+                }
+            ]);
         });
     });
 }
